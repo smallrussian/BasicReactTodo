13b5a92db45603a58ee2cfdec41c185eb179c907
smallrussian <julian.shniter@gmail.com>
2023-05-12 13:16:23 -0500

    added App.css

---
+++ src/features/contact/contactSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const contactSlice = createSlice({
+    name: 'contact',
+    initialState: {
+        contact: []
+    },
+    reducers: {
+        addContact: (state, action) =>{
+            state.contact.push(action.payload)
+        }
+    }
+})
+
+export const {addContact} = contactSlice.actions;
+export default contactSlice.reducer;
